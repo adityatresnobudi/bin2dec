590d34c9def2d190fae77f4613085850de1bbe9f
Aditya Tresnobudi <adityatresnobudi@gmail.com>
2024-03-04 15:58:11 +0700

    implementing error if input has number other than 0 and 1

--- cli/utility.go
+++ cli/utility.go
@@ -7,6 +7,10 @@ import (
 
 func HandleConvert(input string) (int, error) {
 	binConverter := app.NewBin2DecConverter(&entity.Input{Binary: input})
+	valid, err := binConverter.IsValid()
+	if !valid {
+		return 0, err
+	}
 	res, err := binConverter.Convert()
 	return *res, err
 }
